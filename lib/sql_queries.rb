858d12b63ef18cc3b56e6f56a72e83338b458161
Shami Tomita <shamitomita@gmail.com>
2021-03-24 18:25:31 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -27,7 +27,7 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, 
-   projects.funding_goal - SUM(pledges.amount) AS 'over_amount'
+   SUM(pledges.amount) - projects.funding_goal AS 'over_amount'
    FROM projects 
    JOIN pledges 
    ON projects.id = pledges.project_id
